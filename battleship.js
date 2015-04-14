0feaf721ba365b696c0cfd42fc96674e9dbcc538
Tommyluo <tluo12321@yahoo.com>
2015-04-13 23:19:14 -0700

    added views

--- battleship.js
+++ battleship.js
@@ -1,7 +1,27 @@
 'use strict';
 
-$(document).ready(init);
+$(document).ready();
 
-function init() {
+var view = {
+  //takes a string msg and displays it in messageArea
+  displayMessage: function(msg) {
+    var message = $('#messageArea');
+    message.text(msg);
+  },
+  displayHit: function(location) {
+    var td = document.getElementById(location);
+    td.setAttribute('class', 'hit');
+  },
+  displayMiss: function(location) {
+    var td = document.getElementById(location);
+    td.setAttribute('class', 'miss');
+  }
+};
 
-}
+// view.displayMessage("BattleSHIP!!!!!");
+// view.displayMiss("00");
+// view.displayHit("34");
+// view.displayMiss("55");
+// view.displayHit("12");
+// view.displayMiss("25");
+// view.displayHit("26");
